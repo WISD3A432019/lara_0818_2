13233e95433fa0e5f05d11133fcebe39d778b38f
jkluio132465 <jkluio132465@gmail.com>
2017-11-23 01:21:21 +0800

    建立controller並改寫

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('home', function () {
-    return view('home.index');
-});
+Route::get('home', ['as'=>'home.index','uses'=>'HomeController@index']);
+
 
